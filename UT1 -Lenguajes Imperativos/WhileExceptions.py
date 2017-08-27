ae5f194cca2b21251a14e05f35e2448b4c0b4eb7
Joaquin Galletto <JOAQUIN.GALLETTO@correo.ucu.edu.uy>
2017-08-27 17:05:42 -0300

    Added 2 Exceptions

--- UT1 -Lenguajes Imperativos/WhileExceptions.py
+++ UT1 -Lenguajes Imperativos/WhileExceptions.py
@@ -16,6 +16,10 @@ class NumException(WhileSyntaxException):
     def __init__(self, message):
         super(NumException, self).__init__(message)
         
+class VarException(WhileSyntaxException):
+    def __init__(self, message):
+        super(VarException, self).__init__(message)        
+
 class VariableNotDeclaredException(WhileSyntaxException):
     def __init__(self, message):
         super(StmtException, self).__init__(message)
