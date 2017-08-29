d6702a695811a046d953a0c52bf75d26726cf0cd
Joaquin Galletto <JOAQUIN.GALLETTO@correo.ucu.edu.uy>
2017-08-29 16:43:23 -0300

    Deleted 2 exceptions

--- UT1 -Lenguajes Imperativos/WhileExceptions.py
+++ UT1 -Lenguajes Imperativos/WhileExceptions.py
@@ -16,14 +16,6 @@ class StmtException(WhileSyntaxException):
     def __init__(self, message, ):
         super(StmtException, self).__init__(message)
 
-class NumException(WhileSyntaxException):
-    def __init__(self, message):
-        super(NumException, self).__init__(message)
-
-class VarException(WhileSyntaxException):
-    def __init__(self, message):
-        super(VarException, self).__init__(message)
-
 class VariableNotDeclaredException(Exception):
     def __init__(self, message):
         super(VariableNotDeclaredException, self).__init__(message)
