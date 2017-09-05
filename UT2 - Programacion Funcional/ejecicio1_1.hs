2f21e6165932218b8ee8a99639cbb2138b19817d
Pio dos Santos <pio.dossantos@correo.ucu.edu.uy>
2017-09-05 18:07:12 -0300

    Ejercicio 1.1

---
+++ UT2 - Programacion Funcional/ejecicio1_1.hs
@@ -0,0 +1,15 @@
+data Expresion = Numero Int |Suma Expresion Expresion |Producto Expresion Expresion
+  deriving (Eq)
+
+instance Show Expresion where
+  show a = toString a
+
+evaluate::Expresion -> Int
+evaluate (Numero n)=n
+evaluate (Suma a b)= (evaluate a) + (evaluate b)
+evaluate (Producto a b)= (evaluate a) * (evaluate b)
+
+toString::Expresion -> [Char]
+toString (Numero n)=show n
+toString (Suma a b)= "(" ++ (toString a) ++ " + " ++ (toString b) ++ ")"
+toString (Producto a b)= "(" ++  (toString a) ++ " * " ++ (toString b)  ++ ")"
