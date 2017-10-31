f403f33a2405af4a71c1f866d6e4a6e5232c5e34
manubla <manubla4@hotmail.com>
2017-10-31 20:48:06 -0300

    OopsBoolean

--- UT4 - Programacion Orientada a Objetos/HelloWorld/src/OopsBoolean.java
+++ UT4 - Programacion Orientada a Objetos/HelloWorld/src/OopsBoolean.java
@@ -7,12 +7,55 @@
 /**
  *
  * @author pio
- */
-public class OopsBoolean extends OopsObject{
+*/
 
+public class OopsBoolean extends OopsObject {
+  
+    private boolean valor;
+    
+    
     @Override
     public OopsObject respond(String selector, OopsObject[] argumentos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        OopsObject result = null;
+        switch (selector){
+            case "negacion":
+                result = negacion();
+                break;
+            case "and":
+                result = and (((OopsBoolean) argumentos[0]).getValor());
+                break;
+            case "or":
+                result = or (((OopsBoolean) argumentos[0]).getValor());
+                break;
+        }
+        return result;
     }
+    
+    public OopsBoolean (boolean valor){
+        this.valor = valor;
+    }
+    
+    public boolean getValor (){
+        return valor;
+    }
+    
+    
+    public OopsBoolean negacion(){
+        return new OopsBoolean(!(this.getValor()));
+    }
+    
+    public OopsBoolean and (boolean b){
+        return new OopsBoolean((this.getValor()) && b);
+    }
+    
+    
+    public OopsBoolean or (boolean b){
+        return new OopsBoolean((this.getValor()) || b);
+    }
+    
+    
+    
+    
+    
     
 }
